6eae10fc0b5fd0da937eaf48afcab2bb25a4b859
Yiting Xiao <yitingxiao0531@gmail.com>
2024-03-08 16:28:48 -0500

    create new user route working

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,26 +2,24 @@ const db = require("../models/connectToDB");
 
 const usersController = {};
 
-usersController.getUsersInfo = async(req, res, next) => {
+usersController.getUsersInfo = async (req, res, next) => {
  const queryString = `SELECT * FROM userinfo`;
  const users = await db.query(queryString);
- console.log('users', users.rows)
+ console.log(users)
+ const usersInfo = users.rows;
+ res.locals.usersInfo = usersInfo;
+ next();
 }
-// usersController.postNewUser = (req, res, next) => {
-//   console.log("req.body ", req.body);
-//   const { id, username, password } = req.body;
-//   const queryString = `INSERT INTO userinfo (id, username, password)
-//   VALUES (3, 'Erika', 567)`;
-  
-//   db.query(queryString).then((data) => {
-//     // console.log("data from query ", data);
-//     if (data.rowCount >= 1) {
-//       res.locals.payload = data.rows[0];
-//       next();
-//     } else {
-//       next({ err: "Problem creating new user in database" });
-//     }
-//   });
-// };
+
+usersController.postNewUser = async (req, res, next) => {
+  const id = Math.floor(Math.random() * 1000);
+  const values = [ `${id}`, 'dopey', 9090]
+  const queryString = `INSERT INTO userinfo (id, username, password) VALUES ($1, $2, $3)`;
+  const request = await db.query(queryString, values);
+  console.log('request', request);
+  const message = 'successfully created a user';
+  res.locals.message = message;
+  next();
+};
 
 module.exports = usersController;
