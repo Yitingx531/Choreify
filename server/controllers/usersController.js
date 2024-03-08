9856dbd1a2fca92e9aca7ed785c1b1e37c9dc6f0
Yiting Xiao <yitingxiao0531@gmail.com>
2024-03-08 14:53:09 -0500

    connect server to database

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,30 +2,24 @@ const db = require("../models/connectToDB");
 
 const usersController = {};
 
-usersController.postNewUser = (req, res, next) => {
-  console.log("req.body ", req.body);
-  const { id, username, password } = req.body;
-  const queryString = `INSERT INTO userinfo (id, username, password)
-  VALUES (3, 'Erika', 567)`;
+usersController.getUsersInfo = async(req, res, next) => {
+ const queryString = `SELECT * FROM userinfo`;
+ const users = await db.query(queryString);
+ console.log('users', users.rows)
+}
+// usersController.postNewUser = (req, res, next) => {
+//   console.log("req.body ", req.body);
+//   const { id, username, password } = req.body;
+//   const queryString = `INSERT INTO userinfo (id, username, password)
+//   VALUES (3, 'Erika', 567)`;
   
-  db.query(queryString).then((data) => {
-    // console.log("data from query ", data);
-    if (data.rowCount >= 1) {
-      res.locals.payload = data.rows[0];
-      next();
-    } else {
-      next({ err: "Problem creating new user in database" });
-    }
-  });
-};
-
-// usersController.getUsers = (req, res, next) => {
-//   db.query(queries.getAllUsers).then((data) => {
-//     if (data) {
-//       res.locals.payload = data.rows;
+//   db.query(queryString).then((data) => {
+//     // console.log("data from query ", data);
+//     if (data.rowCount >= 1) {
+//       res.locals.payload = data.rows[0];
 //       next();
 //     } else {
-//       next({ err: "Problem fetching users from database" });
+//       next({ err: "Problem creating new user in database" });
 //     }
 //   });
 // };
--- server/models/connectToDB.js
+++ server/models/connectToDB.js
@@ -1,20 +1,16 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres',
+    database: 'ChoreifyData',
     password: '0531',
     port: 5432,
   });
 
-// create a new pool here using the connection string above
-
-// We export an object that contains a property called query,
-// which is a function that returns the invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
 module.exports = {
   query: (text, params, callback) => {
-    console.log('executed query', text);
+    console.log('executed query hello', text);
     return pool.query(text, params, callback);
   }
 };
